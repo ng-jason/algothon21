c34b1857c94df1608349f00ede0e28dab43256a1
ng-jason <nghyphenjason@gmail.com>
2021-07-13 23:33:09 +1000

    Update SixteenFourtySeven.py

--- SixteenFourtySeven.py
+++ SixteenFourtySeven.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
+
 import numpy as np
 from numpy.polynomial.polynomial import polyfit
 import math
 
 nInst=100
 currentPos = np.zeros(nInst)
+stop_loss_value = np.zeros(nInst) # for normal buy-sell
+trailing_profit = np.zeros(nInst) # can be substituted by trailing sl
+posLimit = 10000
+commRate = 0.0050
+
 
 def getMyPosition (prcSoFar):
     global currentPos
-    (nins,nt) = prcSoFar.shape 
+    global stop_loss_values
+    global trailing_profit
+    (nins,nt) = prcSoFar.shape #(100, prices
     rpos = np.zeros(100)
+
+    # Get convergence values
     convergence_values = get_convergence_values(prcSoFar)   
+    #print(f"\n {convergence_values} \n")
     for i in range(0, nins):
+        #if i != 98:
+        #    continue
         cnvg = convergence_values[i]
         new_price = prcSoFar[i][-1]
-        if new_price < cnvg:
-            if currentPos[i] == 0: 
-                qty = ((10000 - (10000 % new_price)) / new_price)
-                qty = math.floor(qty)
-                rpos[i] = qty
-            if currentPos[i] < 0:
-                qty = currentPos[i]
-                qty = math.floor(qty)
-                rpos[i] = rpos[i] - qty 
-        if new_price > cnvg:
-            if currentPos[i] > 0:
-                qty = currentPos[i]
-                qty = math.floor(qty)
-                qty_sll = ((10000 - (10000 % new_price)) / new_price)
-                qty_sll = math.floor(qty_sll)
-                rpos[i] = -qty_sll - qty
-    currentPos += rpos
+        
+        if new_price < (1 - commRate) * cnvg:  # go long
+            rpos[i] = posLimit // new_price
+        elif new_price > (1 + commRate) * cnvg:  # go short
+            rpos[i] = -posLimit // new_price
+        else:
+            rpos[i] = currentPos[i]  # no change
+
+    currentPos = rpos
+    
     return currentPos
 
 def get_convergence_values(prcSoFar):
@@ -53,8 +59,13 @@ def get_convergence_values(prcSoFar):
         for seq in sequences:
             x = np.array(x_values)
             y = np.array(seq)
+            
             m, b = polyfit(x, y, 1)
             pred = 69 * b + m
+            ''' Why does second degree polynomial perform worse ??????
+            a, b, c = polyfit(x, y, 2) # try second degree regression
+            pred = a + b * 69 + c * 69 * 69
+            '''
             tmp_convergence_values.append(pred)
         cnvg = 0
         for val in tmp_convergence_values:
